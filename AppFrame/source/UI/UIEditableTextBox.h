209aeab1b4fc25c451b304eedeca636d69fd083b
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2023-05-21 21:31:18 +0900

    EditorbleTextBoxにデフォルト文字表示機能追加。文字編集機能実装。

--- AppFrame/source/UI/UIEditableTextBox.h
+++ AppFrame/source/UI/UIEditableTextBox.h
@@ -28,15 +28,29 @@ namespace AppFrame {
              * \brief入力モードをオンにする
              */
             void ActivateInput();
+
+            /**
+             * \brief 非表示モード(文字が伏せられる)を設定する
+             * \param flag オンオフ
+             */
+            void SetSecret(bool flag) { _secret = flag; }
+            bool GetSecret() { return _secret; }
+
+            std::string GetText() { return _text; }
+
+            void SetDefaultText(std::string text) { _defaultText = text; }
         private:
+            std::string _defaultText;//入力が無い場合の表示テキスト
             std::string _text;
             int _font;
+            unsigned int _defaultTextColor;
             unsigned int _textColor;
             bool _drawBox;//文字の背景ボックスを描画するか
             unsigned int _boxColor;
 
             int _inputHandle;//キー入力バッファ
             bool _activate;//入力受付中
+            bool _secret;//伏字モード
         };
     }
 }
